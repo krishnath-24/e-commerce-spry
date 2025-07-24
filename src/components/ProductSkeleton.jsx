e3d0c009c582862e40326327fc287d7d30878016
Anuj Sharma <sharma.anuj2498@gmail.com>
2025-07-24 11:36:47 +0530

    update skeleton width

--- src/components/ProductSkeleton.jsx
+++ src/components/ProductSkeleton.jsx
@@ -2,7 +2,7 @@ import { HeartIcon as Heart } from "@heroicons/react/24/outline";
 
 export default function ProductSkeleton() {
   return (
-    <div className="border rounded p-4 shadow w-[300px] relative animate-pulse flex flex gap-2">
+    <div className="border rounded p-4 shadow min-w-[300px] relative animate-pulse flex flex gap-2">
       <Heart className="absolute top-2 right-2 text-gray-300 rounded-full w-6 h-6" />
       <div className="bg-gray-300 rounded w-[100px] h-[100px] mb-2 mx-auto" />
       <div className="flex flex-col gap-2 flex-1 align-left">
